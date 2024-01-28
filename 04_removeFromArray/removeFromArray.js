3273ead1abb785a2808fd2797f3e21f200a1a229
Vadim J. <136094345+shy-Vadim@users.noreply.github.com>
2024-01-28 23:40:09 +0300

    OK

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+  let arrayOfWords = arguments[0];
+  for (i = 1 ; i < arguments.length ; i++) {
+    arrayOfWords = arrayOfWords.filter((a) => a !== arguments[i])
+  }
+  return arrayOfWords;
 };
 
 // Do not edit below this line
