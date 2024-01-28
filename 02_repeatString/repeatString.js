bd824050a3ad019975228e56bbd43111e4258bbd
Vadim J. <136094345+shy-Vadim@users.noreply.github.com>
2024-01-28 20:27:01 +0300

    OK

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, timesToRepeat) {
+  let stringToReturn = "";
+  if (timesToRepeat < 0) {
+    stringToReturn = "ERROR";
+  }
+  for (i = 1 ; i <= timesToRepeat ; i++) {
+    stringToReturn += givenString;
+  }
+  return(stringToReturn);
 };
 
 // Do not edit below this line
