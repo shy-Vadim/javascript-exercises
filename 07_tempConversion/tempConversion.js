389b46faf7587cb36596b9885fd0eae9b09091a5
Vadim J. <136094345+shy-Vadim@users.noreply.github.com>
2024-01-29 16:18:48 +0300

    OK

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  return Number(((far - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Number((cel * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
